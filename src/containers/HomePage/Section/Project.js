d63ddf7ac4f9d7d1c421e5c6245f35c11387c43c
Tuananh1811 <tanhdo18@gmail.com>
2023-02-06 15:34:52 +0700

    update

--- src/containers/HomePage/Section/Project.js
+++ src/containers/HomePage/Section/Project.js
@@ -9,7 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 import projectslide from "../../../assets/projectslide/3d.jpg";
 class Project extends Component {
 
-   
+
     render() {
         let settings = {
             dots: false,
@@ -21,30 +21,34 @@ class Project extends Component {
 
         return (
             <div className='section-project'>
-                <div className='project-content'>
+                <div className='project-container'>
+                    <div className='project-header'>
+                        <span>Dự án mới nhất</span>
+                        <a className='project-header-link' href='/'>Xem thêm</a>
+                    </div>
                     <Slider {...settings}>
                         <div className='pro-customize'>
-                            <img alt='project 1' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 1' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                         <div className='pro-customize'>
-                            <img alt='project 2' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 2' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                         <div className='pro-customize'>
-                            <img alt='project 3' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 3' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                         <div className='pro-customize'>
-                            <img alt='project 4' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 4' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                         <div className='pro-customize'>
-                            <img alt='project 5' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 5' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                         <div className='pro-customize'>
-                            <img alt='project 6' className='pro-customize-img' src={projectslide}/>
+                            <img alt='project 6' className='pro-customize-img' src={projectslide} />
                             <h5 className='pro-customize-title'>Khu liên hợp công nghiệp </h5>
                         </div>
                     </Slider>
