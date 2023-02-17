e0b8db4f380bb2a5126268319af7e803d2db34e6
Tuananh1811 <tanhdo18@gmail.com>
2023-02-17 16:57:47 +0700

    update

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
+import { LANGUAGES } from '../../utils';
 import './Header.scss';
 
 class Header extends Component {
-
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+        //fire redux event:action
+    }
     render() {
         const { processLogout } = this.props;
-
+        let language = this.props.language;
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -21,8 +24,14 @@ class Header extends Component {
                 {/* nút logout */}
                 <div className="btn btn-logout" onClick={processLogout} title="Log out">
                     <div className="languages">
-                        <span className="language-vi">VN</span>
-                        <span className="language-en">EN</span>
+                        <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
+                            onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                            VN
+                        </span>
+                        <span className={language === LANGUAGES.EN ? "language-en active" : "language-vi"}
+                            onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                            EN
+                        </span>
                     </div>
                     <i className="fas fa-sign-out-alt"></i>
                 </div>
@@ -34,13 +43,15 @@ class Header extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        processLogout: () => dispatch(actions.processLogout()),
+        // processLogout: () => dispatch(actions.processLogout()),
+        changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language))
     };
 };
 
