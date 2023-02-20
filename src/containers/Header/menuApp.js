6f612cb1c7adea0d75024e44f693d7470e735192
Tuananh1811 <tanhdo18@gmail.com>
2023-02-20 09:53:27 +0700

    update

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -6,7 +6,6 @@ export const adminMenu = [
                 subMenus: [
                     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
                 ]
             },
             // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
