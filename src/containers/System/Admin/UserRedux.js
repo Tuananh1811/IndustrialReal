6619b5908765b2e256e1d8ca7424d22e03f468b4
Tuananh1811 <tanhdo18@gmail.com>
2023-03-14 11:29:55 +0700

    form manage user use redux

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -15,59 +15,84 @@ class UserRedux extends Component {
     render() {
         return (
             <div className='user-redux-container'>
-                <div className='title'>
-                    react redux
+                <div className='title my-3'>
+                    <FormattedMessage id="manage-user.add" />
                 </div>
                 <div className="user-redux-body" >
                     <div className='container' >
                         <div className='row'>
                             <form>
-                                <div className="row">
+                                <div className="row my-2" >
                                     <div className="form-group col-md-6">
-                                        <label for="inputEmail4">Email</label>
-                                        <input type="email" className="form-control" id="inputEmail4" />
+                                        <label > <FormattedMessage id="manage-user.email" /></label>
+                                        <input type="email" className="form-control" />
                                     </div>
                                     <div className="form-group col-md-6">
-                                        <label for="inputPassword4">Password</label>
-                                        <input type="password" className="form-control" id="inputPassword4" />
+                                        <label > <FormattedMessage id="manage-user.password" /></label>
+                                        <input type="password" className="form-control" />
                                     </div>
+
                                 </div>
-                                <div className="form-group">
-                                    <label for="inputAddress">Address</label>
-                                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-                                </div>
-                                <div className="form-group">
-                                    <label for="inputAddress2">Address 2</label>
-                                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                                </div>
-                                <div className="row">
+                                <div className='row my-2'>
                                     <div className="form-group col-md-6">
-                                        <label for="inputCity">City</label>
-                                        <input type="text" className="form-control" id="inputCity" />
+                                        <label > <FormattedMessage id="manage-user.firstname" /></label>
+                                        <input type="text" className="form-control" />
                                     </div>
-                                    <div className="form-group col-md-4">
-                                        <label for="inputState">State</label>
-                                        <select id="inputState" className="form-control">
-                                            <option selected>Choose...</option>
-                                            <option>...</option>
+                                    <div className="form-group col-md-6">
+                                        <label > <FormattedMessage id="manage-user.lastname" /></label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className='row my-2'>
+                                    <div className="form-group col-6">
+                                        <label > <FormattedMessage id="manage-user.address" /></label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="form-group col-6">
+                                        <label > <FormattedMessage id="manage-user.phonenumber" /></label>
+                                        <input type="text" className="form-control" placeholder="" />
+                                    </div>
+                                </div>
+
+                                <div className="row my-2">
+
+                                    <div className="form-group col-md-3">
+                                        <label > <FormattedMessage id="manage-user.gender" /></label>
+                                        <select className="form-control form-select ">
+
+                                            <option selected> Male</option>
+                                            <option >Female </option>
+                                            <option>... </option>
                                         </select>
                                     </div>
-                                    <div className="form-group col-md-2">
-                                        <label for="inputZip">Zip</label>
-                                        <input type="text" className="form-control" id="inputZip" />
+                                    <div className="form-group col-md-3">
+                                        <label > <FormattedMessage id="manage-user.position" /></label>
+                                        <select className="form-control form-select ">
+
+                                            <option selected>Male</option>
+                                            <option>Female</option>
+                                            <option>Other</option>
+                                        </select>
                                     </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" id="gridCheck" />
-                                        <label className="form-check-label" for="gridCheck">
-                                            Check me out
-                                        </label>
+                                    <div className="form-group col-md-3">
+                                        <label ><FormattedMessage id="manage-user.role" /></label>
+                                        <select className="form-control form-select ">
+
+                                            <option selected>Male</option>
+                                            <option>Female</option>
+                                            <option>Other</option>
+                                        </select>
                                     </div>
+                                    <div className="form-group col-3">
+                                        <label > <FormattedMessage id="manage-user.image" /></label>
+                                        <input type="text" className="form-control" placeholder="" />
+                                    </div>
+
                                 </div>
-                                <button type="submit" className="btn btn-primary">Sign in</button>
+                                <br></br>
+                                <button type="submit" className="btn btn-primary"><FormattedMessage id="manage-user.save" /></button>
                             </form>
-                        </div> 
+                        </div>
                     </div>
                 </div>
             </div>
